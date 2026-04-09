80d192bfa5b5ed6e2b16d89f016f3941e446abe0
jorgesantos001 <patrociniojorgeluiz@gmail.com>
2026-04-09 19:33:51 -0300

    chore(api): registra rotas de pedidos no app

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,7 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Coloca debaixo dessa linha aqui a rotda(route) de orders quando ficar pronto'
+
+import orderRoutes from './routes/order.routes.js';
+app.use(orderRoutes);
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'API online!' });
